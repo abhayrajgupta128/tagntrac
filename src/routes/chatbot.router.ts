d7b3b45a5edfdfcd96d04c1820572fba452e4971
RISHAV <rishavsaha24042003@gmail.com>
2024-11-24 01:50:24 +0530

    removed auth middleware form chatbot route

--- src/routes/chatbot.router.ts
+++ src/routes/chatbot.router.ts
@@ -4,7 +4,6 @@ import { AuthMiddleware } from '../middlewares';
 
 const chatRouter = Router();
 
-chatRouter.use('/tenant', AuthMiddleware.authenticateApiKey);
 chatRouter.get('/tenant/:tenantId/ask', ChatBotController.answerQuestion);
 
 export default chatRouter;
